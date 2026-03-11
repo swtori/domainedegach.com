d19c4a81fec45473035f5e3b90d2c2b113eb58d4
satori <137929088+swtori@users.noreply.github.com>
2026-03-11 10:26:26 +0100

    Create gachDb.sql

---
+++ php/gachDb.sql
@@ -0,0 +1,44 @@
+-- ######################################################
+-- # 📦 gachDb.sql
+-- # 
+-- # Auteur      : author
+-- # Date        : 2024-06-21
+-- # Description : Initialisation de la base de données
+-- #               pour le site Domaine de Gach.
+-- ######################################################
+-- # 📦 gachDb.sql
+-- # Tables :
+-- # - CLIENTS : Utilisateurs du site
+-- # - CHAMBRES : Chambres du site
+-- # - RESERVATIONS : Réservations des chambres
+-- ######################################################
+
+CREATE DATABASE IF NOT EXISTS gachDb;
+USE gachDb;
+
+CREATE TABLE CLIENTS (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    tel VARCHAR(20) NOT NULL,
+    email VARCHAR(70) NOT NULL UNIQUE
+);
+
+CREATE TABLE CHAMBRES (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    designation VARCHAR(255) NOT NULL,
+    prix DECIMAL(10,2) NOT NULL,
+    capaciteMax INT NOT NULL
+);
+
+CREATE TABLE RESERVATIONS (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    dateIn DATE NOT NULL,
+    dateOut DATE NOT NULL,
+    idChambre INT NOT NULL,
+    idClient INT NOT NULL,
+    FOREIGN KEY (idClient) REFERENCES CLIENTS(id) ON DELETE CASCADE,
+    FOREIGN KEY (idChambre) REFERENCES CHAMBRES(id) ON DELETE CASCADE,
+    CHECK (dateOut > dateIn)
+);
+
